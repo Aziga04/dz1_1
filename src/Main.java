115274b0e078aa8aab6144f3eb45325a58edcb99
Aziga04 <azizmusaev33@gmail.com>
2022-09-03 01:10:31 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String doc;
+        final int NUM = 54;
+        String WORD = "bone";
+        doc = NUM + WORD;
+        System.out.println(doc);
+        if (NUM < 0){
+            System.out.println("сохранил отрицател ");
+        }else if (NUM > 0){
+            System.out.println("вы сохранил  отрицател число");
+
+        }else {
+            System.out.println("вы сохранили 0");
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите ваше имя");
+        String user = scan.nextLine ();
+
+
+
+
+    }
+}
